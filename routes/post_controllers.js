302eed6fef713cc851bcbb6680a2d986ed97a360
abba8521 <abba8521@gmail.com>
2022-01-13 13:31:20 +0000

    v1

---
+++ routes/post_controllers.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+const PostModel = require("../models/post_model");
+const ObjectID = require('mongoose').Types.ObjectId;
+
+
+// READ
+router.get('/', (req, res) => {
+    PostModel.find((err, docs) => {
+        if(!err) {
+            res.send(docs)
+        }else{
+            console.log(`erreur: ${err}`)
+        }
+    })
+})
+
+//POST
+router.post("/", (req, res) => {
+    const newContact = new PostModel({
+        nom: req.body.nom,
+        prenom: req.body.prenom,
+        email: req.body.email,
+        age: req.body.age,
+    });
+    newContact.save((err, docs) => {
+        if(!err) {
+            res.send(docs)
+        }else(
+            console.log(`erreur: ${err}`)
+        )
+    })
+})
+
+//PUT
+router.put('/:id', (req, res) => {
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(400).send(`ID inconnu : ${req.params.id}`)
+    }else{
+        const updateContact = {
+            prenom: req.body.prenom,
+        };
+        PostModel.findByIdAndUpdate(
+            req.params.id,
+            {$set: updateContact},
+            {new: true},
+            (err, docs) => {
+                if(!err){
+                    res.send(docs);
+                }else{
+                    res.send(`update error: ${err}`);
+                }
+            }
+        )
+    }
+})
+
+//DELETE
+router.delete('/:id', (req, res) => {
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(400).send(`ID inconnu: ${req.params.id}`)
+    }else{
+        PostModel.findByIdAndRemove(
+            req.params.id,
+            (err, docs) => {
+                (!err) ? res.send(docs) : res.send(`Delete error: ${err}`)
+            }
+        )
+    }
+});
+
+module.exports = router;
